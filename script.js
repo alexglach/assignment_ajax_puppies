0dfea0697acc3b402a6f21536c6150cd353ef94c
Leo Ahnn <leo.ahnn@gmail.com>
2016-09-16 18:56:17 -0400

    working on posting

--- script.js
+++ script.js
@@ -2,36 +2,68 @@
 // seperate function to convert JSON into HTML elements
 // need listener for the refresh
 // document ready that has the initial functions
-// 
+//
 
-var addRefreshListener = function() {
-  $('#refresh-puppies').click(function(){
-    var puppyObject = sendGetRequest();
-    puppyObject.then(
-      function(){
-        var updatedPuppies = puppyObject.responseJSON;
-        updatePuppyList(updatedPuppies);
-      }
-    );
-  });
-};
+LISTENER = {
+  addRefresh: function() {
+    $('#refresh-puppies').click(function() {
+      var puppyObject = REQUEST.get();
+      puppyObject.then(
+        function() {
+          var updatedPuppies = puppyObject.responseJSON;
+          VIEW.updatePuppyList(updatedPuppies);
+        }
+      );
+    });
+  },
 
-var sendGetRequest = function(){
-  return $.get('https://ajax-puppies.herokuapp.com/puppies.json');
-};
-
-var updatePuppyList = function(puppyList) {
-  $list = $("#puppy-list")
-  $.each(puppyList, function(index, puppy) {
-    var created_at = new Date(puppy.created_at);
-    var minutesAgo = $.now() - created_at;
-    var newPuppy = $('<li>')
-                  .html(puppy.name + "(" + puppy.breed + "), created " + minutesAgo + "minutes ago -- ");
-  });
+  addPost: function() {
+    $("#submit-puppy").click(function() {
+      var puppyInfo = VIEW.submitPuppyInfo();
+      REQUEST.post(puppyInfo);
+    });
+  }
 }
 
 
 
-$('document').ready(function(){
-  addRefreshListener();
+
+
+REQUEST = {
+  get: function() {
+    return $.get('https://ajax-puppies.herokuapp.com/puppies.json');
+  },
+
+  post: function(puppyInfo) {
+    $.post("https://ajax-puppies.herokuapp.com/puppies.json", puppyInfo, null, "json")
+  }
+}
+
+
+VIEW = {
+  submitPuppyInfo: function() {
+    return $('form').serialize();
+  },
+
+  updatePuppyList: function(puppyList) {
+    $list = $("#puppy-list")
+    $.each(puppyList, function(index, puppy) {
+      var created_at = new Date(puppy.created_at);
+      var minutesAgo = Math.floor(($.now() - created_at) / 60000);
+      $adoptLink = $("<a>")
+        .attr("data-id", puppy.id)
+        .attr("href", "#")
+        .html("Adopt!")
+      var $newPuppy = $('<li>')
+        .html(puppy.name + "(" + puppy.breed.name + "), created " + minutesAgo + " minutes ago -- ")
+        .append($adoptLink);
+      $newPuppy.prependTo($list);
+    });
+  }
+}
+
+
+$('document').ready(function() {
+  LISTENER.addRefresh();
+  LISTENER.addPost();
 });
